8206049b03b853ef9bfa85ea8aeeaec1a8fedd4e
jaytanet <jaytanet@gmail.com>
2023-12-25 22:55:46 +0700

    update2

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import { AuthProvider } from './auth/AuthContext'; // แก้ไข path นี้
@@ -9,6 +9,7 @@ const App = () => {
     <AuthProvider>
       <Router>
         <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
         </Routes>
